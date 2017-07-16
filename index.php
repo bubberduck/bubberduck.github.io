f3b5cddd3cd9128ca427826cc701519dc58f3bc1
bubberduck <jleadbetterjones@gmail.com>
2017-07-16 15:51:13 +0100

    None

---
+++ index.php
@@ -0,0 +1,15 @@
+ <!DOCTYPE html>
+<html>
+<head>
+<title>Home</title>
+
+<!-- The PHP script below is used to tell the navbar which page is active. -->
+<?php $page = "home"; ?>
+
+</head>
+<body>
+
+<?php include 'header.php'; ?>
+
+</body>
+</html> 
